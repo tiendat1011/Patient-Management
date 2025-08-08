933695436e5057a2bb88dada5a84c0b889977a87
tiendat1011 <tiendat9tc@gmail.com>
2025-08-08 15:57:09 +0700

    feat: add Patient entity, set up profile test, and in-memory h2 db for testing

---
+++ patient-service/src/main/java/dev/proj/patientservice/model/Patient.java
@@ -0,0 +1,30 @@
+package dev.proj.patientservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String address;
+
+    private LocalDate birthDate;
+
+    private LocalDate registeredDate;
+}
