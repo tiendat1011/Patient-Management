9c80f0ebb657cbc2d97ccfc8326dff1cf488eb74
tiendat1011 <tiendat9tc@gmail.com>
2025-08-29 16:02:38 +0700

    feat: update service impl, add new exception

--- patient-service/src/main/java/dev/proj/patientservice/service/PatientService.java
+++ patient-service/src/main/java/dev/proj/patientservice/service/PatientService.java
@@ -4,8 +4,10 @@ import dev.proj.patientservice.dto.request.PatientRequestDTO;
 import dev.proj.patientservice.dto.response.PatientResponseDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PatientService {
     List<PatientResponseDTO> getPatients();
     PatientResponseDTO createPatient(PatientRequestDTO  patientRequestDTO);
+    PatientResponseDTO updatePatient(UUID uuid, PatientRequestDTO  patientRequestDTO);
 }
