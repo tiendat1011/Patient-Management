37ae9b62a46a8c3dc999e2bb6adb3538428629c1
tiendat1011 <tiendat9tc@gmail.com>
2025-08-29 16:03:42 +0700

    fix: remove unuse exception

--- patient-service/src/main/java/dev/proj/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/dev/proj/patientservice/controller/PatientController.java
@@ -2,14 +2,11 @@ package dev.proj.patientservice.controller;
 
 import dev.proj.patientservice.dto.request.PatientRequestDTO;
 import dev.proj.patientservice.dto.response.PatientResponseDTO;
-import dev.proj.patientservice.exception.BadRequestException;
 import dev.proj.patientservice.service.PatientService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
